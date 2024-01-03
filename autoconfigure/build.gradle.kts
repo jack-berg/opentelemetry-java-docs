b1b11a3ad7451d35d2cd138d9468c171cbf5b744
Jack Berg <jberg@newrelic.com>
2024-01-03 10:27:25 -0600

    Demonstrate usage of opentelemetry-exporter-sender-grpc-managed-channel with autoconfigure

--- autoconfigure/build.gradle.kts
+++ autoconfigure/build.gradle.kts
@@ -15,4 +15,10 @@ dependencies {
     implementation("io.opentelemetry:opentelemetry-api")
     implementation("io.opentelemetry:opentelemetry-exporter-logging")
     implementation("io.opentelemetry:opentelemetry-sdk-extension-autoconfigure")
+    implementation("io.opentelemetry:opentelemetry-exporter-otlp") {
+        exclude("io.opentelemetry", "opentelemetry-exporter-sender-okhttp")
+    }
+    implementation("io.opentelemetry:opentelemetry-exporter-sender-grpc-managed-channel")
+    implementation(platform("io.grpc:grpc-bom:1.60.1"))
+    implementation("io.grpc:grpc-netty")
 }
--- autoconfigure/src/main/java/io/opentelemetry/example/autoconfigure/AutoConfigExample.java
+++ autoconfigure/src/main/java/io/opentelemetry/example/autoconfigure/AutoConfigExample.java
@@ -1,9 +1,15 @@
 package io.opentelemetry.example.autoconfigure;
 
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
 import io.opentelemetry.api.OpenTelemetry;
 import io.opentelemetry.api.trace.Span;
 import io.opentelemetry.api.trace.Tracer;
+import io.opentelemetry.exporter.otlp.trace.OtlpGrpcSpanExporter;
+import io.opentelemetry.sdk.OpenTelemetrySdk;
 import io.opentelemetry.sdk.autoconfigure.AutoConfiguredOpenTelemetrySdk;
+
+import java.util.concurrent.TimeUnit;
 
 /**
  * An example of using {@link io.opentelemetry.sdk.autoconfigure.AutoConfiguredOpenTelemetrySdk} and
@@ -14,7 +20,16 @@ public final class AutoConfigExample {
 
   public static void main(String[] args) throws InterruptedException {
     // Let the SDK configure itself using environment variables and system properties
-    OpenTelemetry openTelemetry = AutoConfiguredOpenTelemetrySdk.initialize().getOpenTelemetrySdk();
+    OpenTelemetrySdk openTelemetry = AutoConfiguredOpenTelemetrySdk.builder()
+            .addSpanExporterCustomizer((spanExporter, configProperties) -> {
+              if (spanExporter instanceof OtlpGrpcSpanExporter) {
+                spanExporter.shutdown().join(10, TimeUnit.SECONDS);
+                ManagedChannel managedChannel = ManagedChannelBuilder.forAddress("localhost", 4317).userAgent("foo").usePlaintext().build();
+                return ((OtlpGrpcSpanExporter) spanExporter).toBuilder().setChannel(managedChannel).build();
+              }
+              return spanExporter;
+            })
+            .build().getOpenTelemetrySdk();
 
     AutoConfigExample example = new AutoConfigExample(openTelemetry);
     // Do some real work that'll emit telemetry
